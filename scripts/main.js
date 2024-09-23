a5749c8044f9b69ad00103aae2f694263ee4ddfd
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-23 05:09:25 +0200

    sprite logic setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 const canvas = document.getElementById("main-canvas");
 const ctx = canvas.getContext("2d");
+
+const playerSprite = new Image();
+const vampLadySpritePath = "Idle1.png";
+playerSprite.src = vampLadySpritePath;
+
+AniamteSprite();
+
+function AniamteSprite() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(spriteSheet, 100, 100, 64, 80);
+  requestAnimationFrame(AniamteSprite);
+}
