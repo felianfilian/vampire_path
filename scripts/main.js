1c5e20694bdfb8f8b910695af25e02ab13af7697
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-30 11:06:14 +0200

    main class creation

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,30 @@
 const canvas = document.getElementById("main-canvas");
 const ctx = canvas.getContext("2d");
 
-const idleFrames = [
-  "Idle1.png",
-  "Idle2.png",
-  "Idle3.png",
-  "Idle4.png", // Add more frames as needed
-];
-
-let playerSprite = new Image();
-let currentFrame = 0;
-let frameCount = idleFrames.length;
-let frameDelay = 10; // Control the speed of animation (higher is slower)
-let frameTicker = 0; // Ticker to manage delay between frames
+const playerSprite = new Image();
+const playerIdlePath = "assets/vampire-lady/Idle1.png";
+playerSprite.src = playerIdlePath;
 
 let player;
+
+class Main {
+  constructor() {
+    player = new Player();
+    this.AnimateSprite();
+  }
+
+  AnimateSprite() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+      playerSprite,
+      player.position.x,
+      player.position.y,
+      player.size.width,
+      player.size.height
+    );
+    requestAnimationFrame(() => this.AnimateSprite());
+  }
+}
 
 class Player {
   constructor() {
@@ -29,32 +39,4 @@ class Player {
   }
 }
 
-window.onload = function () {
-  main();
-};
-
-function main() {
-  player = new Player();
-  AnimateSprite();
-}
-
-function AnimateSprite() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Update the sprite frame based on frameTicker
-  frameTicker++;
-  if (frameTicker > frameDelay) {
-    currentFrame = (currentFrame + 1) % frameCount;
-    playerSprite.src = idleFrames[currentFrame]; // Change to the next frame
-    frameTicker = 0;
-  }
-
-  ctx.drawImage(
-    playerSprite,
-    player.position.x,
-    player.position.y,
-    player.size.width,
-    player.size.height
-  );
-  requestAnimationFrame(AniamteSprite);
-}
+let main = new Main();
